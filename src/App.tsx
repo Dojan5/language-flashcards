b9e62f545dc6ea9d5f9437a86f799a897e96d547
Joel Lundborg (Agio) <jolu1@agio.se>
2022-05-22 15:06:38 +0200

    Remove test buttons

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,6 @@ const App = () => {
         <FlashcardContainer>
           <Flashcard />
         </FlashcardContainer>
-        <div>
-          <Button icon={<span>1</span>}>Help</Button>
-          <Button color="red">Thanks, button!</Button>
-          <Button color="blue">Badonkadonk</Button>
-          <Button color="green" icon={<span className="material-symbols-outlined">light_mode</span>}>
-            This is another button
-          </Button>
-          <Button color="orange" icon={<span>5</span>}>
-            This is another button
-          </Button>
-        </div>
       </Content>
     </>
   )
